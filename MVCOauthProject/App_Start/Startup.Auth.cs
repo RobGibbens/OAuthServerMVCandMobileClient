90b453fbc9e2c2a0fa01aebebdec7cd063a27a69
Jason D <gitkraken@deboever.us>
2016-06-08 09:34:23 -0700

    MVCOauthProject added

---
+++ MVCOauthProject/App_Start/Startup.Auth.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Security.Claims;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.Owin;
+using Microsoft.Owin.Security.Cookies;
+using Microsoft.Owin.Security.Google;
+using Owin;
+using MVCOauthProject.Models;
+using Microsoft.Owin.Security.OAuth;
+using System.Threading.Tasks;
+using Microsoft.Owin.Security;
+using Microsoft.Owin.Security.Infrastructure;
+
+namespace MVCOauthProject
+{
+    public partial class Startup
+    {
+        // For more information on configuring authentication, please visit http://go.microsoft.com/fwlink/?LinkId=301864
+        public void ConfigureAuth(IAppBuilder app)
+        {
+            // Configure the db context, user manager and signin manager to use a single instance per request
+            app.CreatePerOwinContext(ApplicationDbContext.Create);
+            app.CreatePerOwinContext<ApplicationUserManager>(ApplicationUserManager.Create);
+            app.CreatePerOwinContext<ApplicationSignInManager>(ApplicationSignInManager.Create);
+
+            // Enable the application to use a cookie to store information for the signed in user
+            // and to use a cookie to temporarily store information about a user logging in with a third party login provider
+            // Configure the sign in cookie
+            app.UseCookieAuthentication(new CookieAuthenticationOptions
+            {
+                AuthenticationType = DefaultAuthenticationTypes.ApplicationCookie,                
+                LoginPath = new PathString("/Account/Login"),
+                Provider = new CookieAuthenticationProvider
+                {
+                    // Enables the application to validate the security stamp when the user logs in.
+                    // This is a security feature which is used when you change a password or add an external login to your account.  
+                    OnValidateIdentity = SecurityStampValidator.OnValidateIdentity<ApplicationUserManager, ApplicationUser>(
+                        validateInterval: TimeSpan.FromMinutes(30),
+                        regenerateIdentity: (manager, user) => user.GenerateUserIdentityAsync(manager))
+                }
+            });
+            app.UseExternalSignInCookie(DefaultAuthenticationTypes.ExternalCookie);
+
+            // Enables the application to temporarily store user information when they are verifying the second factor in the two-factor authentication process.
+            //app.UseTwoFactorSignInCookie(DefaultAuthenticationTypes.TwoFactorCookie, TimeSpan.FromMinutes(5));
+
+            // Enables the application to remember the second login verification factor such as phone or email.
+            // Once you check this option, your second step of verification during the login process will be remembered on the device where you logged in from.
+            // This is similar to the RememberMe option when you log in.
+            //app.UseTwoFactorRememberBrowserCookie(DefaultAuthenticationTypes.TwoFactorRememberBrowserCookie);
+
+            app.UseOAuthAuthorizationServer(new OAuthAuthorizationServerOptions
+            {
+                AuthorizeEndpointPath = new PathString("/OAuth2/Authorize"),
+                TokenEndpointPath = new PathString("/OAuth2/Token"),
+                ApplicationCanDisplayErrors = true,
+#if DEBUG
+                AllowInsecureHttp = true,
+#endif
+                // Authorization server provider which controls the lifecycle of Authorization Server
+                Provider = new OAuthAuthorizationServerProvider
+                {
+                    OnValidateClientRedirectUri = ValidateClientRedirectUri,
+                    OnValidateClientAuthentication = ValidateClientAuthentication
+                   , OnAuthorizeEndpoint = AuthEndPoint
+
+                    //OnGrantResourceOwnerCredentials = GrantResourceOwnerCredentials,
+                    //OnGrantAuthorizationCode = GrantAuthorizationCode,                   
+                },
+                // Authorization code provider which creates and receives authorization code
+                AuthorizationCodeProvider = new AuthenticationTokenProvider
+                {
+                    OnCreate = CreateAuthenticationCode,
+                    OnReceive = ReceiveAuthenticationCode
+                },
+
+            });
+            
+            app.UseGoogleAuthentication(new Microsoft.Owin.Security.Google.GoogleOAuth2AuthenticationOptions
+               {
+                   ClientId = "856383537530-tbj2verubre3g9gsk921ql9gtqpef5ah.apps.googleusercontent.com",
+                   ClientSecret = "9YpexQCawdBvHc9RuNKTm-oR",
+                   //CallbackPath = new PathString("/AuthorizationServer/Account/External"),
+
+               });
+        
+        }
+
+        private async Task AuthEndPoint(OAuthAuthorizeEndpointContext context)
+        {
+            var authentication = context.OwinContext.Authentication;
+            var userManager = context.OwinContext.GetUserManager<ApplicationUserManager>();
+
+            //if (authentication.AuthenticateAsync(DefaultAuthenticationTypes.ApplicationCookie).Result?.Identity == null)
+            if ( ! context.Request.User.Identity.IsAuthenticated)
+            {
+                authentication.Challenge(DefaultAuthenticationTypes.ApplicationCookie);
+                context.RequestCompleted();
+                return;
+            }
+
+            //Since we also do OAuth, we have to sign in the OAuth Identification Type as well
+            var user = userManager.FindByName(context.Request.User?.Identity?.Name);
+
+            ClaimsIdentity oAuthIdentity = await userManager.CreateIdentityAsync(user, OAuthDefaults.AuthenticationType);
+
+            authentication.SignIn(oAuthIdentity);
+            context.RequestCompleted();
+            
+            return;
+        }
+
+        private ConcurrentDictionary<string, string> _authenticationCodes = new ConcurrentDictionary<string, string>(); 
+        private void CreateAuthenticationCode(AuthenticationTokenCreateContext context)
+        {
+            context.SetToken(Guid.NewGuid().ToString("n") + Guid.NewGuid().ToString("n"));
+            _authenticationCodes[context.Token] = context.SerializeTicket();
+        }
+
+        private void ReceiveAuthenticationCode(AuthenticationTokenReceiveContext context)
+        {
+            string value;
+            if (_authenticationCodes.TryRemove(context.Token, out value))
+            {
+                context.DeserializeTicket(value);
+            }
+        }
+
+        private Task ValidateClientAuthentication(OAuthValidateClientAuthenticationContext context)
+        {
+            string AcceptableClientId = "A8375B66";
+            string AcceptableClientSecret = "A32D8C3CBE9A";
+
+            string clientId;
+            string clientSecret;
+            if (context.TryGetBasicCredentials(out clientId, out clientSecret) ||
+                context.TryGetFormCredentials(out clientId, out clientSecret))
+            {
+                if (clientId == AcceptableClientId && clientSecret == AcceptableClientSecret)
+                {
+                    context.Validated();
+                }
+            }
+            return Task.FromResult(0);
+        }
+
+        private Task ValidateClientRedirectUri(OAuthValidateClientRedirectUriContext arg)
+        {
+            //if (context.ClientId == "A8375B66")
+                arg.Validated();
+
+            return Task.FromResult(0);
+        }
+    }
+}
